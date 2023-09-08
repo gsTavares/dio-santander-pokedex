0683bd5b35a5822180ff976381c1ec3760344cba
gsTavares <gustavo.tavares24@outlook.com>
2023-09-08 00:37:51 -0300

    feat: fetching pokemon list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
-console.log("Sucesso!");
+const offset = 0;
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+const pokemonListElement = document.getElementById("pokemonList");
+
+const convertPokemonToLi = (pokemon) => {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types"> 
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        for(pokemon of pokemonList) {
+            pokemonListElement.innerHTML += convertPokemonToLi(pokemon);
+        }
+    })
+    .catch((error) => console.log(error));
