83826beed01e0dbb7436eb0e8d78b6a2240370d5
gsTavares <gustavo.tavares24@outlook.com>
2023-09-08 21:45:59 -0300

    feat: getting more data from pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,20 @@ const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 const pokemonListElement = document.getElementById("pokemonList");
 
+const convertPokemonTypesToLi = (types) => {
+    return types.map(typeSlot => `<li class="type">${typeSlot.type.name}</li>`).join("");
+}
+
 const convertPokemonToLi = (pokemon) => {
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types"> 
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types)}
                 </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.sprites.other.dream_world.front_default}"
                     alt="${pokemon.name}">
             </div>
         </li>
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,17 @@
 const pokeApi = {
-    getPokemons: (offset = 0, limit = 10) => {
+    getPokemonDetail: async (pokemon) => {
+        return fetch(pokemon.url)
+            .then((response) => response.json());
+    },
+
+    getPokemons: async (offset = 0, limit = 5) => {
         const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
         return fetch(url)
             .then((response) => response.json())
             .then((jsonBody) => jsonBody.results)
+            .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+            .then((detailRequests) => Promise.all(detailRequests))
+            .then((pokemonsDetails) => pokemonsDetails)
             .catch((error) => console.error(error));
     }
 }
